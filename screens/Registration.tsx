5a394f3a3320dbf1113126597b84b9e54af4634f
AKFakuade <akfakuade99@gmail.com>
2024-06-07 00:15:02 -0400

    first commit

--- screens/Registration.tsx
+++ screens/Registration.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, Button, TouchableOpacity, TextInput, Linking, Dimensions } from "react-native";
+import { Alert, StyleSheet, Text, View, Image, Button, TouchableOpacity, TextInput, Linking, Dimensions } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import supabase from "../config/supabaseClient.js";
-import { v4 as uuidv4 } from "uuid";
 
 const height = Dimensions.get('window').height;
 
@@ -17,13 +16,11 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 24,
         color: 'white',
-        fontFamily: 'DM Sans-Regular',
         fontWeight: 'bold'
     },
     buttonText: {
         fontSize: 20,
         color: 'white',
-        fontFamily: 'DM Sans-Regular',
         fontWeight: 'bold',
         alignSelf: 'center'
     },
@@ -36,7 +33,7 @@ const styles = StyleSheet.create({
         borderRadius: 2
     },
     input: {
-        top: 120,
+        top: 100,
         margin: 14,
         borderWidth: 1,
         padding: 12,
@@ -45,7 +42,6 @@ const styles = StyleSheet.create({
         borderTopWidth: 0,
         width: 350,
         fontSize: 20,
-        fontFamily: 'DM Sans-Regular',
         fontWeight: '700',
         textAlign: 'left',
         alignSelf: 'center',
@@ -81,14 +77,13 @@ const styles = StyleSheet.create({
     text3: {
         color: '#7A7373',
         alignSelf: 'center',
-        top: 210,
+        top: 160,
     },
     text4: {
         color: '#7A7373',
         fontSize: 12,
         alignSelf: 'center',
-        fontFamily: 'DMSans-Regular',
-        top: -height * 0.225,
+        top: -50,
     },
     text5: {
         color: '#EA4335',
@@ -96,7 +91,6 @@ const styles = StyleSheet.create({
         left: 116,
         top: 250,
         fontSize: 14,
-        fontFamily: 'DMSans-Regular',
     },
     text6: {
         top: 50,
@@ -115,42 +109,45 @@ export default function Registration() {
     const [email, setEmail] = useState("");
     const [password, setPass] = useState("");
     const [confirm, setConfirm] = useState("");
-    const [userID, setUserID] = useState<string | null>(null);
-    const [formError, setFormError] = useState(null);
+    const [phone_number, setphone_number] = useState("");
     const [passError, setPassError] = useState(false);
-    const id = uuidv4();
+    const [loading, setLoading] = useState(false);
+
     const showPassError = () => {
         setPassError(true);
     };
-    // const SignUp = async () => {
-    //     console.log("?")
-    //     let { data, error } = await supabase.auth.signUp({
-    //         email: email,
-    //         password: password,
-    //     })
-    // }
+
     const checkText = async (e: { preventDefault: () => void; }) => {
         e.preventDefault()
-        if (!email || !password || !confirm || password !== confirm) {
+        if (!email || !password || !confirm || !phone_number || password !== confirm) {
             showPassError();
             return
         }
         navigation.navigate("Name")
+        setLoading(true);
+        const {
+            data: { session },
+        } = await supabase.auth.signUp({
+            email,
+            password,
+        });
         const { data, error, } = await supabase
             .from('User')
-            .insert([{ id, email, password }])
+            .insert([{ email, password, phone_number }])
             .select()
 
         if (error) {
+            Alert.alert(error.message);
             console.log(error)
         }
         if (data) {
             console.log(data)
-            setFormError(null)
+            if (!session)
+                Alert.alert("Please check your inbox for email verification!");
+            setLoading(false);
         }
-
-
     }
+
 
     return (
         <View style={{ backgroundColor: "white", width: '100%', height: '100%' }}>
@@ -166,13 +163,20 @@ export default function Registration() {
             />
             <TextInput
                 style={styles.input}
+                value={phone_number}
+                placeholder="Enter Phone Number"
+                placeholderTextColor="#7A7373"
+                onChangeText={setphone_number}
+            />
+            <TextInput
+                style={styles.input}
                 secureTextEntry={true}
                 value={password}
                 placeholderTextColor="#7A7373"
                 onChangeText={setPass}
                 placeholder={"Enter Password "} />
             <TextInput
-                style={styles.input}
+                style={[styles.input, { top: 120 }]}
                 secureTextEntry={true}
                 value={confirm}
                 placeholderTextColor="#7A7373"
@@ -183,7 +187,8 @@ export default function Registration() {
                 onPress={checkText}>
                 <Text style={styles.buttonText}>Sign Up</Text>
             </TouchableOpacity>
-            <TouchableOpacity
+
+            {/* <TouchableOpacity
                 style={styles.sign}
                 onPress={() => console.log("google log in")}>
                 <Image
@@ -203,8 +208,8 @@ export default function Registration() {
                 <Image
                     source={{ uri: 'https://i.imgur.com/QQPTpll.png' }}
                     style={{ width: 250, height: 50, margin: -10 }} />
-            </TouchableOpacity>
-            <View
+            </TouchableOpacity> */}
+            {/* <View
                 style={{
                     borderBottomColor: '#F28627',
                     borderBottomWidth: StyleSheet.hairlineWidth,
@@ -223,8 +228,8 @@ export default function Registration() {
                     left: 221,
                     top: 25,
                 }}
-            />
-            <Text style={styles.text2}>or</Text>
+            /> */}
+            {/* <Text style={styles.text2}>or</Text> */}
             <Text style={styles.text3}>Already Have an Account? <Text style={{
                 textDecorationLine: 'underline',
                 color: '#7A7373',
